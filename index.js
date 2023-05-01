daf17c3a5fb4571f9c751946342a461ee347494b
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-01 22:54:55 +0600

    initial port runing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    
+    res.send('Hello , the port is running on 5000. ');
+
+
+});
+
+
+app.listen(port, () => {
+    
+console.log('The port is runing on:',port);
+
+})

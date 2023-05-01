169acf982ec68cb25b468ef20a91bb1501470d09
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-01 23:28:59 +0600

    Middle wire added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-
+const cors = requre('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.get('/', (req, res) => {
     
-    res.send('Hello , the port is running on 5000. ');
+    res.send('Hello , the port is running on 5000.');
 
 
 });

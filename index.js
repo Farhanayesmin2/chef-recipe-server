deb7b45a1a1264cc900e64a38db773b41f9f80d2
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-04 10:50:47 +0600

    Set the port and chef route

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const cors = requre('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+// Get the data
+const chef = require('./data/chef.json');
+
+
+// Set running data or not
 app.get('/', (req, res) => {
-    
-    res.send('Hello , the port is running on 5000.');
+    res.send('api running in port 5000')
+})
 
-
+// Send data of chef
+app.get('/chef', (req, res) => {
+    res.send(chef)
 });
 
 

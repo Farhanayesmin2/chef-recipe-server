f4e31dcfcb3e0f192e6e98692e95efe7e09c0295
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-05-04 15:32:15 +0600

    Specific chef add

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 // Get the data
 const chef = require('./data/chef.json');
+const recipes = require('./data/recipes.json');
 
 
 // Set running data or not
@@ -16,6 +17,21 @@ app.get('/', (req, res) => {
 // Send data of chef
 app.get('/chef', (req, res) => {
     res.send(chef)
+});
+
+// // Get Specific id from Categories.
+// app.get('/recipes/:id', (req, res) => {
+//     const id = req.params.id;
+    
+//         const chefRecipes = recipes.filter(n => n.category_id === id);
+//         res.send(chefRecipes);
+    
+// });
+//All Specific chef
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedchef = recipes.filter(n => n.category_id === id);
+    res.send(selectedchef)
 });
 
 
